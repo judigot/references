4d8eea521982db9120bdd0f125105c0ec4242c0a
judigot <judigot@gmail.com>
2024-02-15 13:59:58 +0800

    Update

--- tempCodeRunnerFile.ps1
+++
@@ -1,7 +0,0 @@
-
-$response = Invoke-WebRequest -Uri 'https://github.com/judigot/references/blob/main/Apportable.sh'
-
-$html = $response.ParsedHtml
-$elementValue = $html.getElementById('read-only-cursor-text-area').innerHTML
-
-echo $html
