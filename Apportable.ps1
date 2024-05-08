e75a770c7c2e5edf702d61da00ad9b46881f5413
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-05-08 10:32:26 +0800

    Update

--- Apportable.ps1
+++ Apportable.ps1
@@ -23,7 +23,7 @@ export PATH="`$PATH:$($pathsLinux)"
 export NVM_HOME="$("$portableGitInstallationDir/nvm" -replace '\n', ':' -replace '\\', '/' -replace 'C:', '/c')"
 export NVM_SYMLINK="$("$portableGitInstallationDir/nodejs" -replace '\n', ':' -replace '\\', '/' -replace 'C:', '/c')"
 
-[[ -f ".snippetsrc" ]] && source ".snippetsrc"
+[[ -f "`$HOME/.snippetsrc" ]] && source "`$HOME/.snippetsrc"
 "@
 Set-Content -Path $file_path -Value $file_content
 Write-Host "$filename created successfully at: $file_path"
