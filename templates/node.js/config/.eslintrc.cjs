7d97523ad07588dccc77d64a243bf1b7294e11ea
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-06-03 15:28:28 +0800

    Add config files

---
+++ templates/node.js/config/.eslintrc.cjs
@@ -0,0 +1,78 @@
+module.exports = {
+    root: true,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+  
+      'plugin:@typescript-eslint/strict-type-checked', // Very strict!
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:react-hooks/recommended',
+      'plugin:jsx-a11y/recommended',
+      //
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      'react-refresh',
+      //
+      'react',
+      '@typescript-eslint',
+      'react-hooks',
+      'jsx-a11y',
+      //
+    ],
+    //
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+      project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.custom.json'],
+      tsconfigRootDir: __dirname,
+    },
+    //
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+  
+      //
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'TSEnumDeclaration',
+          message: 'Enums are not allowed. Use object literals instead.',
+        },
+      ],
+      'no-alert': ['error'],
+      'no-console': ['error', { allow: ['warn', 'error'] }], // Disable all console outputs except console.warn and console.error
+      'react/react-in-jsx-scope': 'off',
+      // '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': ['error'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { args: 'all', argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'error',
+      // complexity: ['error', 10],
+      // 'max-depth': ['error', 4],
+      // 'max-lines': ['error', 300],
+      'react/jsx-props-no-spreading': 'error',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  };
+  
