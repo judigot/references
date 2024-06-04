b4d8dc33a28c4b79affe4c2d06b75c5a04f2cae5
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-06-04 14:26:26 +0800

    Update

--- templates/node.js/backend/prisma/database/schema.sql
+++ templates/node.js/backend/prisma/database/schema.sql
@@ -1,11 +1,18 @@
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(32) NOT NULL UNIQUE,
-    email VARCHAR(32) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ DEFAULT NULL
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT,
+    gender TEXT
 );
+-- CREATE TABLE users (
+--     id BIGSERIAL PRIMARY KEY,
+--     username VARCHAR(32) NOT NULL UNIQUE,
+--     email VARCHAR(32) NOT NULL UNIQUE,
+--     password_hash TEXT NOT NULL,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+--     deleted_at TIMESTAMPTZ DEFAULT NULL
+-- );
