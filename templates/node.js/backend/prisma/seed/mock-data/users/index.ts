5985aede1fa386cfe009f3c22af72c38b86d7d31
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-06-04 14:40:00 +0800

    Update

---
+++ templates/node.js/backend/prisma/seed/mock-data/users/index.ts
@@ -0,0 +1,43 @@
+import { users_1_to_5000 } from './Users (1 - 5000)';
+import { users_5001_to_10000 } from './Users (5001 - 10000)';
+import { users_10001_to_15000 } from './Users (10001 - 15000)';
+import { users_15001_to_20000 } from './Users (15001 - 20000)';
+import { users_20001_to_25000 } from './Users (20001 - 25000)';
+import { users_25001_to_30000 } from './Users (25001 - 30000)';
+import { users_30001_to_35000 } from './Users (30001 - 35000)';
+import { users_35001_to_40000 } from './Users (35001 - 40000)';
+import { users_40001_to_45000 } from './Users (40001 - 45000)';
+import { users_45001_to_50000 } from './Users (45001 - 50000)';
+import { users_50001_to_55000 } from './Users (50001 - 55000)';
+import { users_55001_to_60000 } from './Users (55001 - 60000)';
+import { users_60001_to_65000 } from './Users (60001 - 65000)';
+import { users_65001_to_70000 } from './Users (65001 - 70000)';
+import { users_70001_to_75000 } from './Users (70001 - 75000)';
+import { users_75001_to_80000 } from './Users (75001 - 80000)';
+import { users_80001_to_85000 } from './Users (80001 - 85000)';
+import { users_85001_to_90000 } from './Users (85001 - 90000)';
+import { users_90001_to_95000 } from './Users (90001 - 95000)';
+import { users_95001_to_100000 } from './Users (95001 - 100000)';
+
+export const users = [
+...users_1_to_5000,
+...users_5001_to_10000,
+...users_10001_to_15000,
+...users_15001_to_20000,
+...users_20001_to_25000,
+...users_25001_to_30000,
+...users_30001_to_35000,
+...users_35001_to_40000,
+...users_40001_to_45000,
+...users_45001_to_50000,
+...users_50001_to_55000,
+...users_55001_to_60000,
+...users_60001_to_65000,
+...users_65001_to_70000,
+...users_70001_to_75000,
+...users_75001_to_80000,
+...users_80001_to_85000,
+...users_85001_to_90000,
+...users_90001_to_95000,
+...users_95001_to_100000
+];
