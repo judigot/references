6d0ff10933494d78b004a3d29c4bb9ee44be9e93
judigot <judigot@gmail.com>
2024-12-21 16:15:31 +0000

    Update

--- BigBangVite.sh
+++ BigBangVite.sh
@@ -1063,9 +1063,6 @@ addVitestConfig() {
         cat <<EOF
 import { defineConfig } from 'vitest/config';
 import { resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   test: {
